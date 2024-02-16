408ac643a89c865a063081a5c1287f3311960b44
carol60131 <carol60131@users.noreply.github.com>
2024-02-16 11:25:51 +0800

    增加判斷是否計算夜間飛行時間
    增加判斷是否計算夜間飛行時間

--- calFlyTime.py
+++ calFlyTime.py
@@ -5,52 +5,68 @@ from datetime import datetime as dt
 import datetime
 
 
-def calFlyTime(liberationDT, arrivalDT, sunriseTime, sunsetTime):
+def calFlyTime(liberationDT, arrivalDT, sunriseTime, sunsetTime, isFix):
 
-	# 出發日期與抵達日期相同(抵達時間-出發時間)
-	if arrivalDT.date() == liberationDT.date():
+	# 不計算夜間時間
+	if isFix == 1:
+
+		# 出發日期與抵達日期相同(抵達時間-出發時間)
+		if arrivalDT.date() == liberationDT.date():
+			result = arrivalDT - liberationDT
+			resMin = result.seconds / 60 
+
+			return resMin		
+
+		else:
+			# 將日出、日落時間與出發時間格式化為同日期
+			sunriseTime = dt.strptime(str(liberationDT.date()) + ' ' + str(sunriseTime.time()), '%Y-%m-%d %H:%M:%S')
+			sunsetTime = dt.strptime(str(liberationDT.date()) + ' ' + str(sunsetTime.time()), '%Y-%m-%d %H:%M:%S')
+
+			# 日落時間-出發時間(第一天)
+			result = sunsetTime - liberationDT
+
+			while True:
+				# 出發、日出、日落時間加一日
+				liberationDT = liberationDT + datetime.timedelta(days=1)
+				sunriseTime = sunriseTime + datetime.timedelta(days=1)
+				sunsetTime = sunsetTime + datetime.timedelta(days=1)
+				
+				if arrivalDT.date() == liberationDT.date():
+					break
+
+				else:
+					# 日落時間-日出時間
+					result += sunsetTime - sunriseTime
+
+			# 抵達時間-日出時間(最後一日)
+			result += arrivalDT - sunriseTime
+			resMin = result.total_seconds() / 60 
+
+			return resMin
+
+	# 計算夜間時間
+	elif isFix == 2:
+
+		# 抵達時間-出發時間
 		result = arrivalDT - liberationDT
-		resMin = result.seconds / 60 
+		resMin = result.total_seconds() / 60 
 
-		return resMin		
-
-	else:
-		# 將日出、日落時間與出發時間格式化為同日期
-		sunriseTime = dt.strptime(str(liberationDT.date()) + ' ' + str(sunriseTime.time()), '%Y-%m-%d %H:%M:%S')
-		sunsetTime = dt.strptime(str(liberationDT.date()) + ' ' + str(sunsetTime.time()), '%Y-%m-%d %H:%M:%S')
-
-		# 日落時間-出發時間(第一天)
-		result = sunsetTime - liberationDT
-
-		while True:
-			# 出發、日出、日落時間加一日
-			liberationDT = liberationDT + datetime.timedelta(days=1)
-			sunriseTime = sunriseTime + datetime.timedelta(days=1)
-			sunsetTime = sunsetTime + datetime.timedelta(days=1)
-			
-			if arrivalDT.date() == liberationDT.date():
-				break
-			else:
-				# 日落時間-日出時間
-				result += sunsetTime - sunriseTime
-
-		# 抵達時間-日出時間(最後一日)
-		result += arrivalDT - sunriseTime
-		resMin = result.seconds / 60 
-
-		return resMin		
+		return resMin
 
 
 def main():
 	# 出發、抵達時間
-	liberationDT = dt.strptime('2024-02-05 07:00:00', '%Y-%m-%d %H:%M:%S')
-	arrivalDT = dt.strptime('2024-02-08 14:30:00', '%Y-%m-%d %H:%M:%S')
+	liberationDT = dt.strptime('2024-02-14 08:00:00', '%Y-%m-%d %H:%M:%S')
+	arrivalDT = dt.strptime('2024-02-17 17:00:00', '%Y-%m-%d %H:%M:%S')
 
 	# 日出、日落時間
 	sunriseTime = dt.strptime('06:00:00', '%H:%M:%S')
-	sunsetTime = dt.strptime('19:00:00', '%H:%M:%S')
+	sunsetTime = dt.strptime('18:00:00', '%H:%M:%S')
 
-	time = calFlyTime(liberationDT, arrivalDT, sunriseTime, sunsetTime)
+	# 判斷是否計算夜間時間
+	isFix = 2
+
+	time = calFlyTime(liberationDT, arrivalDT, sunriseTime, sunsetTime, isFix)
 	print('共飛行', time, '分鐘')
 
 
